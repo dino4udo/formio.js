f6eb1388f435229f7df34a7ac164296ca63c7a60
Daniil <daniil.chumachenko@democrance.com>
2020-12-08 11:42:21 +0400

    lint: Revert utils files and libs

--- docs/script/inherited-summary.js
+++ docs/script/inherited-summary.js
@@ -1,29 +1,28 @@
 (function(){
-    function toggle(ev) {
-        const button = ev.target;
-        let parent = ev.target.parentElement;
-        while(parent) {
-            if (parent.tagName === 'TABLE' && parent.classList.contains('summary')) break;
-            parent = parent.parentElement;
-        }
-
-        if (!parent) return;
-
-        const tbody = parent.querySelector('tbody');
-        if (button.classList.contains('opened')) {
-            button.classList.remove('opened');
-            button.classList.add('closed');
-            tbody.style.display = 'none';
-        }
-        else {
-            button.classList.remove('closed');
-            button.classList.add('opened');
-            tbody.style.display = 'block';
-        }
+  function toggle(ev) {
+    var button = ev.target;
+    var parent = ev.target.parentElement;
+    while(parent) {
+      if (parent.tagName === 'TABLE' && parent.classList.contains('summary')) break;
+      parent = parent.parentElement;
     }
 
-    const buttons = document.querySelectorAll('.inherited-summary thead .toggle');
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', toggle);
+    if (!parent) return;
+
+    var tbody = parent.querySelector('tbody');
+    if (button.classList.contains('opened')) {
+      button.classList.remove('opened');
+      button.classList.add('closed');
+      tbody.style.display = 'none';
+    } else {
+      button.classList.remove('closed');
+      button.classList.add('opened');
+      tbody.style.display = 'block';
     }
+  }
+
+  var buttons = document.querySelectorAll('.inherited-summary thead .toggle');
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', toggle);
+  }
 })();
